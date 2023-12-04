ff3de4662afa72205bf8f32932b869efa0dd5d75
EcaterinaGalatan <151677760+EcaterinaGalatan@users.noreply.github.com>
2023-12-04 17:38:04 +0200

    board.cpp

--- board.cpp
+++ board.cpp
@@ -1,15 +1,15 @@
 #include "board.h"
 
-// Default constructor
+// Конструктор по умолчанию
 Board::Board() : _width(20), _height(20) {}
 
-// Constructor with parameters
+// Конструктор с параметрами
 Board::Board(int width, int height) : _width(width), _height(height) {}
 
-// Copy constructor
+// Конструктор копирования
 Board::Board(const Board &other) : _width(other._width), _height(other._height) {}
 
-// Copy assignment operator
+// Оператор копирования
 Board &Board::operator=(const Board &other) {
     if (this != &other) {
         _width = other._width;
@@ -18,28 +18,28 @@ Board &Board::operator=(const Board &other) {
     return *this;
 }
 
-// Getter function for width
+// Getter function для ширины
 int Board::GetWidth() const {
     return _width;
 }
 
-// Getter function for height
+// Getter function для высоты
 int Board::GetHeight() const {
     return _height;
 }
 
-// Comparison operator
+// Оператор сравнения
 bool Board::operator==(const Board &other) const {
     return (_width == other._width) && (_height == other._height);
 }
 
-// Input operator
+// Оператор ввода
 std::istream &operator>>(std::istream &is, Board &board) {
     is >> board._width >> board._height;
     return is;
 }
 
-// Output operator
+// Оператор вывода
 std::ostream &operator<<(std::ostream &os, const Board &board) {
     os << "Board with width: " << board._width << ", height: " << board._height;
     return os;
