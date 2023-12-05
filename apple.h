6f8c4b0a8b388085e6ff3df30af8408656eda332
EcaterinaGalatan <151677760+EcaterinaGalatan@users.noreply.github.com>
2023-12-05 12:45:02 +0200

    update

--- apple.h
+++ apple.h
@@ -2,68 +2,31 @@
 #include "point.h"
 #include <iostream>
 
-/**
- * @file apple.h
- * @brief Definition of the Apple class.
- * @author Ecaterina Galatan
- * @date 2023
- * @project Snake Game Lab 05
- */
-
 class Apple {
-    Point _position; ///< Position of the Apple.
+    Point _position;
 
 public:
-    /**
-     * @brief Default constructor for Apple.
-     */
+    // Default constructor
     Apple();
 
-    /**
-     * @brief Constructor with parameters for Apple.
-     * @param position The position of the Apple.
-     */
+    // Constructor with parameters
     Apple(const Point &position);
 
-    /**
-     * @brief Copy constructor for Apple.
-     * @param other The Apple object to be copied.
-     */
+    // Copy constructor
     Apple(const Apple &other);
 
-    /**
-     * @brief Copy assignment operator for Apple.
-     * @param other The Apple object to be assigned.
-     * @return A reference to the assigned Apple.
-     */
+    // Copy assignment operator
     Apple &operator=(const Apple &other);
 
-    /**
-     * @brief Comparison operator for Apple.
-     * @param other The Apple object to be compared.
-     * @return True if the Apples are equal, false otherwise.
-     */
+    // Comparison operator
     bool operator==(const Apple &other) const;
 
-    /**
-     * @brief Input operator for Apple.
-     * @param is The input stream.
-     * @param apple The Apple object to be read.
-     * @return The input stream after reading the Apple.
-     */
+    // Input operator
     friend std::istream &operator>>(std::istream &is, Apple &apple);
 
-    /**
-     * @brief Output operator for Apple.
-     * @param os The output stream.
-     * @param apple The Apple object to be written.
-     * @return The output stream after writing the Apple.
-     */
+    // Output operator
     friend std::ostream &operator<<(std::ostream &os, const Apple &apple);
 
-    /**
-     * @brief Getter function to retrieve the position of the Apple.
-     * @return The position of the Apple.
-     */
+    // Getter function
     Point GetPosition() const;
 };
