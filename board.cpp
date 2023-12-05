eb9d1b9b25be8908da7cbb2822f41de879c770f3
EcaterinaGalatan <151677760+EcaterinaGalatan@users.noreply.github.com>
2023-12-05 08:56:04 +0200

    board.cpp

--- board.cpp
+++ board.cpp
@@ -2,22 +2,22 @@
 
 /**
  * @file board.cpp
- * @brief Implementation of the Board class functions.
+ * @brief Реализация функций класса Board.
  * @author Ecaterina Galatan
  * @date 2023
  * @project Snake Game Lab 05
  */
 
-// Default constructor
+// Конструктор по умолчанию
 Board::Board() : _width(20), _height(20) {}
 
-// Constructor with parameters
+// Конструктор с параметрами
 Board::Board(int width, int height) : _width(width), _height(height) {}
 
-// Copy constructor
+// Конструктор копирования
 Board::Board(const Board &other) : _width(other._width), _height(other._height) {}
 
-// Copy assignment operator
+// Оператор присваивания копирования
 Board &Board::operator=(const Board &other) {
     if (this != &other) {
         _width = other._width;
@@ -26,29 +26,29 @@ Board &Board::operator=(const Board &other) {
     return *this;
 }
 
-// Getter function for width
+// Функция-геттер для ширины
 int Board::GetWidth() const {
     return _width;
 }
 
-// Getter function for height
+// Функция-геттер для высоты
 int Board::GetHeight() const {
     return _height;
 }
 
-// Comparison operator
+// Оператор сравнения
 bool Board::operator==(const Board &other) const {
     return (_width == other._width) && (_height == other._height);
 }
 
-// Input operator
+// Оператор ввода
 std::istream &operator>>(std::istream &is, Board &board) {
     is >> board._width >> board._height;
     return is;
 }
 
-// Output operator
+// Оператор вывода
 std::ostream &operator<<(std::ostream &os, const Board &board) {
-    os << "Board with width: " << board._width << ", height: " << board._height;
+    os << "Доска с шириной: " << board._width << ", высотой: " << board._height;
     return os;
 }
